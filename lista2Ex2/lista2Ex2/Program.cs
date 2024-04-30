3ab4d7dd677df9ed34b97e2117c5f4ae5f8c8af9
mmarques-ssz <37246763+mmarques-ssz@users.noreply.github.com>
2024-04-29 21:28:44 -0300

    Add files via upload

---
+++ lista2Ex2/lista2Ex2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista2Ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            Console.Write("Digite o 1º valor: ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Digite o 2º valor: ");
+            b = int.Parse(Console.ReadLine());
+
+            if (a == b)
+            {
+                Console.WriteLine("Iguais");
+            }
+            else
+            {
+                if (a > b)
+                {
+                    Console.WriteLine("Primeiro");
+                }
+                else
+                {
+                    Console.WriteLine("Segundo");
+                }
+            }
+        }
+    }
+}
---
+++ lista2Ex3/lista2Ex3/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista2Ex3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            int c;
+
+            Console.Write("Digite o 1º valor: ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Digite o 2º valor: ");
+            b = int.Parse(Console.ReadLine());
+            Console.Write("Digite o 3º valor: ");
+            c = int.Parse(Console.ReadLine());
+
+            if (a == b)
+                if (a == c)
+                    Console.WriteLine("Iguais");
+                else
+                    if (a > c)
+                       Console.WriteLine("1º/2º");
+                    else
+                       Console.WriteLine("3º");
+            else
+                if (a == c)
+                   if (a > b)
+                      Console.WriteLine("1º/3º");
+                   else
+                      Console.WriteLine("2º");
+                else
+                   if (b == c)
+                      if (b > a)
+                         Console.WriteLine("2º/3º");
+                      else
+                         Console.WriteLine("1º");
+                   else
+                      if (a > b)
+                         if (a > c)
+                            Console.WriteLine("1º");
+                         else
+                            Console.WriteLine("3º");
+                      else
+                         if (b > c)
+                            Console.WriteLine("2º");
+                         else
+                            Console.WriteLine("3º");
+        }
+    }
+}
